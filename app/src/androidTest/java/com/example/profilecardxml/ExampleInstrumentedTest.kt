f5be85471ebeffcb88f78ea96b61518c29ac45dd
naman-cr <76682299+naman-cr@users.noreply.github.com>
2025-01-12 18:20:27 +0530

    updated some code

--- app/src/androidTest/java/com/example/profilecardxml/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/profilecardxml/ExampleInstrumentedTest.kt
@@ -13,6 +13,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+// Github Access token : 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
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
