e2e3b408ab27d294b32fe2b10f9b13080d305c70
naman-cr <76682299+naman-cr@users.noreply.github.com>
2025-01-12 19:23:16 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,17 +36,5 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.cardview:cardview:1.0.0")
-    testImplementation("junit:junit:1.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
-    implementation("com.google.code.gson:gson:1.83.8")
-
-    implementation("com.github.bumptech.glide:glide:1.19.0")
 //    kapt("com.github.bumptech.glide:compiler:4.12.0")
 }
