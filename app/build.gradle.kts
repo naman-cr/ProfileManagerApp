cc00db0ea7a78023f2dfef60b13a6f222a1249f5
naman-cr <76682299+naman-cr@users.noreply.github.com>
2025-01-12 18:46:30 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.google.code.gson:gson:1.83.8")
 
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:1.19.0")
 //    kapt("com.github.bumptech.glide:compiler:4.12.0")
 }
